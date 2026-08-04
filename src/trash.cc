fb9b6f8bd71c2d93663099fbd8a2d76b4d7153e4
Colin Clark <colin.clark@cclark.uk>
2026-08-04 14:03:06 +0100

    Fix monir coding error in move to trash

--- src/trash.cc
+++ src/trash.cc
@@ -183,18 +183,20 @@ gboolean file_util_safe_unlink(const gchar *path)
 		}
 	else
 		{
-		GFile *tmp = g_file_new_for_path(path);
+		g_autoptr(GFile) file = g_file_new_for_path(path);
 		g_autoptr(GError) error = nullptr;
 
-		if (!g_file_trash(tmp, FALSE, &error) )
-			{
-			g_autofree gchar *message = g_strconcat(_("See the Help file for a possible workaround.\n\n"), error->message, NULL);
-			gd = warning_dialog(_("Move to trash failed\n\n"), message, GQ_ICON_DIALOG_ERROR, nullptr);
-			generic_dialog_add_button(gd, GQ_ICON_HELP, _("Help"), move_to_trash_failed_cb, FALSE);
+		success = g_file_trash(file, nullptr, &error);
 
-			/* A second warning dialog is not necessary */
+		if (!success && !gd)
+			{
+			g_autofree gchar *message = g_strdup_printf( "%s\n\n%s", _("See the Help file for a possible workaround."), error ? error->message : _("Unknown error"));
+
+			gd = warning_dialog(_("Move to trash failed"), message, GQ_ICON_DIALOG_ERROR, nullptr);
+
+			generic_dialog_add_button(gd, GQ_ICON_HELP, _("Help"), move_to_trash_failed_cb, FALSE);
 			}
-	}
+		}
 
 	return success;
 }
